96995a0be55ec6ca4f997e1c9a4130b5a01b72d9
Ezequiel Etchecoin <ezequiel.etchecoin@incluit.com>
2023-05-02 18:33:16 -0300

    built up some integration tests for app services

---
+++ src/AssistantBot.Tests/AssistantBotServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using AssistantBot.DataTypes;
+using AssistantBot.Services;
+using AssistantBot.Services.Factories;
+using AssistantBot.Services.RedisStorage;
+using Xunit.Abstractions;
+
+namespace AssistantBot.Tests
+{
+    public class AssistantBotServiceTests
+    {
+        private readonly AssistantBotService _service;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public AssistantBotServiceTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+            _service = new AssistantBotService(
+                new ChatBotServiceFactory().CreateService(ChatBotServiceOption.ChatGpt),
+                new RedisVectorStorageService<EmbeddedTextVector>("localhost:6379"));
+        }
+
+        [Fact]
+        public async Task AskToKnowledgeBase_ValidQuestion()
+        {
+            var result = await _service.AskToKnowledgeBase("de qué color son las rosas?");
+
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+
+            _outputHelper.WriteLine(result);
+        }
+    }
+}
---
+++ src/AssistantBot.Tests/KnowledgeBaseServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using AssistantBot.DataTypes;
+using AssistantBot.Services;
+using AssistantBot.Services.Factories;
+using AssistantBot.Services.RedisStorage;
+using Xunit.Abstractions;
+
+namespace AssistantBot.Tests
+{
+    public class KnowledgeBaseServiceTests
+    {
+        private readonly KnowledgeBaseService _service;
+        private readonly ITestOutputHelper _outputHelper;
+
+        public KnowledgeBaseServiceTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+            _service = new KnowledgeBaseService(
+                new ChatBotServiceFactory().CreateService(ChatBotServiceOption.ChatGpt),
+                new RedisVectorStorageService<EmbeddedTextVector>("localhost:6379"));
+        }
+
+        [Fact]
+        public async Task AddParagraphToKnowledgeBase_PopulateTestBase()
+        {
+            var paragraphs = new List<string>
+            {
+                // enter text here
+            };
+
+            foreach(var paragraph in paragraphs)
+            {
+                _ = await _service.AddParagraphToKnowledgeBase(paragraph);
+            }
+        }
+    }
+}
