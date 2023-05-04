9a5000f84b1382578a6c2fc9aacebe6815339f0f
Ezequiel Etchecoin <ezequiel.etchecoin@incluit.com>
2023-05-04 10:46:33 -0300

    added some examples for data ordering explanation

---
+++ src/AssistantBot.Tests/ExamplesTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit.Abstractions;
+
+namespace AssistantBot.Tests
+{
+    public class ExamplesTests
+    {
+        private readonly List<string> stringListToOrder;
+        private readonly List<int> intListToOrder;
+
+        private readonly ITestOutputHelper _outputHelper;
+
+        public ExamplesTests(ITestOutputHelper outputHelper)
+        {
+            stringListToOrder = new List<string>
+            {
+                "300", "3456", "34", "3555", "30", "35", "351"
+            };
+
+            intListToOrder = stringListToOrder
+                .Select(x => int.Parse(x))
+                .ToList(); // 300,3456...
+
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void StringOrdering()
+        {
+            var orderedStrings = stringListToOrder.OrderBy(x => x);
+
+            foreach(var @string in orderedStrings)
+            {
+                _outputHelper.WriteLine(@string);
+            }
+        }
+
+        [Fact]
+        public void NumericOrdering()
+        {
+            var orderedNumbers = intListToOrder.OrderBy(x => x);
+
+            foreach (var @int in orderedNumbers)
+            {
+                _outputHelper.WriteLine(@int.ToString());
+            }
+        }
+    }
+}
