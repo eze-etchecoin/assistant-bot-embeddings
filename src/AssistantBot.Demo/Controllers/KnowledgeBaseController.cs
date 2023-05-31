354bfd35983324668802110ac2dc09a3dd46e17a
Ezequiel Etchecoin <ezequiel.etchecoin@incluit.com>
2023-05-31 11:31:13 -0300

    knowledgebase controller added

---
+++ src/AssistantBot.Demo/Controllers/KnowledgeBaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssistantBot.Demo.Controllers
+{
+    public class KnowledgeBaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
