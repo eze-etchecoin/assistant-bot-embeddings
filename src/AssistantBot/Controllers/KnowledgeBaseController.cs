b0bdab0a402538b9378bd11d91aebf0b15e215d6
Ezequiel Etchecoin <ezequiel.etchecoin@incluit.com>
2023-06-01 18:28:18 -0300

    added file uploading and processing implementation

--- src/AssistantBot/Controllers/KnowledgeBaseController.cs
+++ src/AssistantBot/Controllers/KnowledgeBaseController.cs
@@ -2,8 +2,9 @@
 using AssistantBot.Common.Interfaces;
 using AssistantBot.Models.KnowledgeBase;
 using AssistantBot.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Net.Http.Headers;
 
 namespace AssistantBot.Controllers
 {
@@ -34,6 +35,66 @@ namespace AssistantBot.Controllers
             {
                 return BadRequest(ex.Message);
             }
+        }
+
+        [HttpPost("/UploadFileToKnowledgeBase")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
+        //[ServiceFilter(typeof(ValidateMimeMultipartContentFilter))]
+        public IActionResult UploadFileToKnowledgeBase(FileDescriptionShort fileDescriptionShort)
+        {
+            var uploadedFilePath = "";
+            //var contentTypes = new List<string>();
+
+            foreach (var file in fileDescriptionShort.File)
+            {
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition)
+                        .FileName.ToString().Trim('"');
+
+                    //contentTypes.Add(file.ContentType);
+
+                    //names.Add(fileName);
+
+                    var extension = Path.GetExtension(Path.GetFileName(fileName));
+
+                    var fileName2 = Path.Combine(".", "UploadedFiles", fileDescriptionShort.Name + extension);
+
+                    //if (formFile == null)
+                    //{
+                    //    throw new ArgumentNullException(nameof(formFile));
+                    //}
+
+                    using var fileStream = new FileStream(fileName2, FileMode.Create);
+                    var inputStream = file.OpenReadStream();
+                    inputStream.CopyTo(fileStream);
+
+                    uploadedFilePath = fileName2;
+                }
+            }
+
+            if(string.IsNullOrEmpty(uploadedFilePath))
+                throw new Exception("No file was uploaded.");
+
+            _ = Task.Run(() => _service.LoadFileToKnowledgeBase(uploadedFilePath), CancellationToken.None);
+
+            return Ok(uploadedFilePath);
+        }
+
+        [HttpGet("GetDocumentProcessingStatus")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
+        public IActionResult GetDocumentProcessingStatus(string filePath)
+        {
+            var progress = _service.GetDocumentProcessingStatus(filePath);
+            return Ok(progress);
+        }
+
+        public class FileDescriptionShort
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public string Name { get; set; }
+            public ICollection<IFormFile> File { get; set; }
         }
     }
 }
