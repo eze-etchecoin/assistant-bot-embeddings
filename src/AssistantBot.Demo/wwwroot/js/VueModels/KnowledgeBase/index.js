b9f208044b18796b6a0fa50994fb9a06931b389b
Fran Radicci <francosoff7@hotmail.com>
2023-06-07 11:36:52 -0300

    knowledgeBase view

---
+++ src/AssistantBot.Demo/wwwroot/js/VueModels/KnowledgeBase/index.js
@@ -0,0 +1,38 @@
+﻿
+
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            Texto: "",
+            isLoading: false,
+            showingAlert: false,
+        };
+    },
+    methods: {
+        guardar() {
+            this.Texto = "";
+            this.showingAlert = false;
+            this.isLoading = true;
+            setTimeout(this.showAlert, 3000);
+            
+            
+        },
+        
+        showAlert() {
+            this.isLoading = false;
+            this.showingAlert = true;
+        }
+
+        
+
+    },
+    computed: {
+        buttonDisabled() {
+            return this.Texto === "";
+        }
+    }
+
+}).mount('#vueContainer');
+
+
