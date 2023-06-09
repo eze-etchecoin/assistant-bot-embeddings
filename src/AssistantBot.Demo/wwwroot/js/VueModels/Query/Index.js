92220f2f0974f53c1f36c83f775cd04acf379e3d
Pilar Montenegro <nadiapilarm@gmail.com>
2023-06-09 11:40:56 -0300

    Http request query

--- src/AssistantBot.Demo/wwwroot/js/VueModels/Query/Index.js
+++ src/AssistantBot.Demo/wwwroot/js/VueModels/Query/Index.js
@@ -15,7 +15,6 @@ createApp({
             textoIngresado: "",
             mostrarRespuesta: false,
             isLoading: false,
-
         };
     },
     computed: {
@@ -24,34 +23,32 @@ createApp({
         }
     },
     methods: {
-        consultar() {
-            this.textoIngresado = this.Texto;
-            this.Texto = "";
+        async consultar() {
+            //this.textoIngresado = this.Texto;
+            //this.Texto = "";
             this.mostrarRespuesta = false;
             this.isLoading = true;
-            setTimeout(this.showAlert, 3000);
+            /*setTimeout(this.showAlert, 3000);*/
 
-            /* // PETICIÓN A LA API
+             // PETICIÓN A LA API
 
             const dataObject = {
                 Question: this.Texto
             };
 
-            const { data } = axios.post(
+            const { data } = await axios.post(
                 `${ApiUrl}/AssistantBot/AskToKnowledgeBase`,
                 dataObject);
 
-            this.textoIngresado = data;
-            this.mostrarRespuesta = true;
-
-            */
-        },
-
-            
-        showAlert() {
             this.isLoading = false;
-            this.mostrarRespuesta = true;
+            this.textoIngresado = data;
+            this.mostrarRespuesta = true;    
         },
+
+        //showAlert() {
+        //    this.isLoading = false;
+        //    this.mostrarRespuesta = true;
+        //},
 
         closeAlert() {
             this.mostrarRespuesta = false;   
