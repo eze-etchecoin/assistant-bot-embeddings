12fe28dba575e259f6fa07ee159d6878b13befc9
Pilar Montenegro <nadiapilarm@gmail.com>
2023-06-23 12:02:48 -0300

    uploaded files index.js

--- src/AssistantBot.Demo/wwwroot/js/VueModels/UploadedFiles/Index.js
+++ src/AssistantBot.Demo/wwwroot/js/VueModels/UploadedFiles/Index.js
@@ -7,19 +7,19 @@ createApp({
             rowsUploadedFiles:
                 [
                     {
-                        "FileName": "document1.pdf",
-                        "UploadedDateTime": "2023-06-23T14:32:01.7334218+00:00",
-                        "TotalParagraphs": 11
+                        FileName: "document1.pdf",
+                        UploadedDateTime: "2023-06-23T14:32:01.7334218+00:00",
+                        TotalParagraphs: 11
                     },
                     {
-                        "FileName": "document2.pdf",
-                        "UploadedDateTime": "2023-06-23T14:35:00.0000000+00:00",
-                        "TotalParagraphs": 40
+                        FileName: "document2.pdf",
+                        UploadedDateTime: "2023-06-23T14:35:00.0000000+00:00",
+                        TotalParagraphs: 40
                     },
                     {
-                        "FileName": "document3.docx",
-                        "UploadedDateTime": "2023-06-23T14:37:00.0000000+00:00",
-                        "TotalParagraphs": 5
+                        FileName: "document3.docx",
+                        UploadedDateTime: "2023-06-23T14:37:00.0000000+00:00",
+                        TotalParagraphs: 5
                     }
                 ]
         };
