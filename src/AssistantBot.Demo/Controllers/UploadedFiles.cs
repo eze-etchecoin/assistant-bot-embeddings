341030d46296b7feba685f3645eee00c3e3e6f0b
Pilar Montenegro <nadiapilarm@gmail.com>
2023-06-23 12:01:54 -0300

    UploadedFiles

---
+++ src/AssistantBot.Demo/Controllers/UploadedFiles.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssistantBot.Demo.Controllers
+{
+    public class UploadedFiles : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
